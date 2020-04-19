5ff31bca9ba2ebe25dab6225b4df19f0406541b7
Raúl Merino <rmerino_88@hotmail.com>
2020-04-19 20:45:40 +0200

    Routing, navbar y footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { SearchComponent } from './components/search/search.component';
 
-
-const routes: Routes = [];
+// El orden en el que se colocan las rutas afecta en el matcheo de la url recibida
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'detail/:id', component: DetailComponent },
+  { path: ':idFilm', component: DetailComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SearchComponent,
+    DetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
